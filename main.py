26a69e245381abb9ffc52a88c2350f4fc7d9e1c8
albina12345 <albina.nikol18@gmail.com>
2023-02-12 09:46:08 +0100

    6 work

---
+++ main.py
@@ -0,0 +1,32 @@
+class Wow:
+    def __wow(self):
+        print("Wow")
+    def _hello(self):
+        print("Hello")
+
+some_obj = Wow()
+some_obj._hello()
+some_obj._Wow__wow()
+
+class Hello:
+    def __init__(self):
+        print("Hello")
+class Hello_World(Hello):
+    def __init__(self):
+        super().__init__()
+        print("Word")
+hello_world = Hello_World
+
+class Grandparent:
+    def about(self):
+        print("I am GrandParent")
+    def about_myself(self):
+        print("I am Grandparent")
+class Parent(Grandparent):
+    def about_myself(self):
+        print("I am Parent")
+class Child(Parent):
+    def __init__(self):
+        super().about()
+        super().about_myself()
+nick = Child()
