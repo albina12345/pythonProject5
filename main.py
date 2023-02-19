f8199f6a6b8e99f9c5b3c088528f495ba0b343b7
albina12345 <albina.nikol18@gmail.com>
2023-02-19 09:06:27 +0100

    666

--- main.py
+++ main.py
@@ -30,3 +30,35 @@ class Child(Parent):
         super().about()
         super().about_myself()
 nick = Child()
+
+
+
+
+
+try:
+    print("start code")
+    print(error)
+    print("No errors")
+except:
+    print("We have an error")
+
+print("code after capsule")
+
+try:
+    print("start code")
+    print("No errors")
+except NameError as error:
+    print(error)
+else:
+    print("I am ELSE")
+finally:
+    print('Finally code')
+
+def checker(var_1):
+    if type (var_1)!= str:
+        raise TypeError(f"Sorry, we can`t work with {type(var_1)}, f"we need class str")
+
+    else:
+        return var_1
+first_var = 10
+checker(first var)
